269faaa760d5e76ef061d047ad2221a7d7379d8e
stanleykuadey <stanley.kuadey@gmail.com>
2023-10-05 14:40:22 -0700

    A C program that prints an exact given text and outputs an error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,9 +13,9 @@
  */
 
 int main(void)
-
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, 59); /* Write the message to stderr */
-	return 1; /* Return 1 to indicate an error */
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
 }
