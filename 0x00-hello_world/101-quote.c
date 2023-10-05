97e15edfdb3304b26b22a47a007410936bf3c970
stanleykuadey <stanley.kuadey@gmail.com>
2023-10-05 14:57:00 -0700

    A C program that prints an exact given text and outputs an error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -14,10 +14,8 @@
 
 int main(void)
 {
-	 /* Define a character array 'quo' to store the message to be printed */
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-/* Use the 'write' function to print the message in 'quo' to standard output (file descriptor 1) */
 	write(1, quo, 59);
-	return (1);}
-	 /* Return the value '1' to indicate an error or abnormal termination */
+	return (1);
+}
