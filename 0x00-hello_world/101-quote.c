da8372607c8e93b1e7934ce716433c170fec27df
stanleykuadey <stanley.kuadey@gmail.com>
2023-10-05 13:36:23 -0700

    A C program that prints an exact given text and outputs an error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -14,19 +14,23 @@
 
 int main(void)
 {
-	/* This line declares an array of characters named quo 
+	/**
+	 * This line declares an array of characters named quo 
 	 * and initializes it with a specific quote enclosed in
 	 * double quotes.
 	*/
+
 	char quo[] = "nd that piece of art is useful" - Dora
 		Korpar, 2015-10-19\n";
 
-	/* This line uses the write function to write the contents
+	/**
+	 * This line uses the write function to write the contents
 	 * of the quo array to the standard output (file descriptor 1).
 	 * It specifies quo as the buffer to write, and 59 as the
 	 * number of bytes to write, which corresponds to the length
 	 * of the quote.
 	*/
+
 	write(1, quo, 59);
 	return (1);
 }
