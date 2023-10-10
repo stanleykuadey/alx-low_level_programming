a69f4d065610bfb96801256512a9d3ab1452494b
stanleykuadey <stanley.kuadey@gmail.com>
2023-10-10 11:42:16 -0700

    Add a scrpt with  a function that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - Check if a character is lowercase.
+ *
+ * @c: The character to check.
+ *
+ * Description: This function checks whether the inut character 'c' is lowercase.
+ *
+ * Return: 1 if 'c' is lowercase, 0 otherwise (Indicating success)
+*/
+
+int _islower(int c)
+{
+	/* Check if 'c' is within the ASCII range of lowercase letters (97-122) */
+	if (c >= 97 && c <= 122)
+	{
+		return (1); /* 'c' is lowercase */
+	}
+
+	return (0); /* 'c' is not lowercase */
+}
