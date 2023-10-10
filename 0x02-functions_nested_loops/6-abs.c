74afb729fec9c7c0cfad7d8abc1d82ce17fdc617
stanleykuadey <stanley.kuadey@gmail.com>
2023-10-10 13:14:12 -0700

    Add a script with a function that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the abslute value of an integer.
+ *
+ * @n: The integer for which to compute the absolute value.
+ *
+ * Description: This function computes the absolute value of the input integer 'n'.
+ *
+ * Return: The absolute value of 'n'.
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (-1) * n; /* Calculate the absolute value if 'n' is negative */
+	}
+	return (n); /* Return the absolute value */
+}
