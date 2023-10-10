e3bbb07515bf6ba89d1ee3bc01e8480d4e3ee2b3
stanleykuadey <stanley.kuadey@gmail.com>
2023-10-10 16:45:54 -0700

    Add a script with a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,20 @@
+#include "../main.h"
+
+/**
+ * main - Entry point
+ *
+ * This function serves as the entry point for the program.
+ * It includes the "main.h" header file and calls the
+ * "jack_bauer" function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	/* Call the jack_bauer function */
+	jack_bauer();
+
+	return(0); /* Return 0 to indicate successful
+		      execution */
+}
