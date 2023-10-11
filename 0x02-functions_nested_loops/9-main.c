b67a97a004ce6746ea8ee5804a9b7b796062bc51
stanleykuadey <stanley.kuadey@gmail.com>
2023-10-10 17:37:18 -0700

    Add a script with a function that prints the 9 times table, starting with 0

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * This function serves as the entry point for the program.
+ * It includes the "main.h" header file and calls the
+ * "times_table" function.
+ *
+ * Return: Always 0 (Indicating successful program execution)
+*/
+
+int main(void)
+{
+	/* Call the times_table function */
+	times_table();
+
+	return (0); /* Return 0 to indicate successful
+		       execution */
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times table
+ *
+ * Description: This function prints the 9 times table in a
+ * specific format. It uses nested loops to calculate and
+ * format the table.
+*/
+
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; ++num)
+	{
+		_putchar(48); /* Print the first digit '0'
+				 before the table. */
+
+		for (mult = 1; mult <= 9; ++mult)
+		{
+			_putchar(','); /* Print a comma and
+					* space before each
+					* product.
+					*/
+			_putchar(' ');
+
+			prod = num * mult;
+
+			/* Format the product for printing */
+			if (prod <= 9)
+				_putchar(' '); /* Put a space
+						* for single-digit
+						* products.
+						*/
+			else
+				_putchar((prod / 10) + 48); /* Print the
+							       first digit. */
+			_putchar((prod % 10) + 48); /* Print the second digit */
+		}
+		_putchar ('\n'); /* Move to the next line after
+				    each row of the table. */
+	}
+}
