c089dfe693bdb3e4d8fc079a8f4cb0573cd4b8a6
stanleykuadey <stanley.kuadey@gmail.com>
2023-10-10 17:17:18 -0700

    Add a script with a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,24 +14,23 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <= 9; b++)
-		{
-			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-			{
-				for (c = 0; c <= 5; c++)
-				{
-					for (d = 0; d <= 9; d++)
-					{
-
-			_putchar(a + '0'); /* Print tens digit of the hour */
-			_putchar(b + '0'); /* Print ones digit of the hour */
-			_putchar(58);
-			_putchar(c + '0'); /* Print tens digit of the minute */
-			_putchar(d + '0'); /* Print ones digit of the minute */
-			_putchar('\n'); /* Print a newline character to create a new line */
-					}
-				}
-			}
-		}
+	for (b = 0; b <= 9; b++)
+	{
+	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+	{
+	for (c = 0; c <= 5; c++)
+	{
+	for (d = 0; d <= 9; d++)
+	{
+		_putchar(a + '0'); /* Print tens digit of the hour */
+		_putchar(b + '0'); /* Print ones digit of the hour */
+		_putchar(58);
+		_putchar(c + '0'); /* Print tens digit of the minute */
+		_putchar(d + '0'); /* Print ones digit of the minute */
+		_putchar('\n'); /* Print a newline character to create a new line */
+	}
+	}
+	}
+	}
 	}
 }
